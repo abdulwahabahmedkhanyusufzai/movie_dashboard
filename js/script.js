03c5168fb8f8cd1fefab793e8f5e162eb5b2761b
abdulwahabahmedkhanyusufzai <absurw@gmail.com>
2024-12-16 20:34:17 +0500

    Update

--- js/script.js
+++ js/script.js
@@ -13,3 +13,22 @@ if(navClose){
         nav.classList.remove('show-menu')
     })
 }
+
+let swiperMovie = new Swiper('.movie__swiper', {
+    loop: true,
+    grabCursor: true,
+    slidesPerView: 2,
+    spaceBetween: 24,
+ 
+    breakpoints:{
+       440: {
+          slidesPerView: 'auto',
+       },
+       768: {
+          slidesPerView: 4,
+       },
+       1200: {
+          slidesPerView: 5,
+       },
+    },
+ })
